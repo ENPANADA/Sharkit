5f144dab8146acd82deb9da0f84d8470be465a22
ENPANADA <92269511+ENPANADA@users.noreply.github.com>
2024-10-11 21:24:02 -0600

    Add Encode to Base64 format

--- src/utils/IconsService.tsx
+++ src/utils/IconsService.tsx
@@ -8,16 +8,27 @@ const langs: PropTSX = {
   javascript: IconJS,
 };
 
-const toData = (Icon: ReactElement, props?: Prop) =>
+export const getIconLangByName = ({ name }: { name: string }) =>
+  langs[name]?.();
+
+const toUtf8 = (Icon: ReactElement, props?: Prop) =>
   `data:image/svg+xml;utf8,${encodeURIComponent(
     ReactDOMServer.renderToStaticMarkup(<Icon {...props} />)
   )}`;
 
-export const getIconLangByName = ({ name }: { name: string }) =>
-  langs[name]?.();
+const toBase64 = (Icon: ReactElement, props?: Prop) =>
+  `data:image/svg+xml;base64,${btoa(
+    ReactDOMServer.renderToStaticMarkup(<Icon {...props} />)
+  )}`;
 
-export const svgToData = (svg: ReactElement, props?: Prop) =>
-  toData(svg, props);
+export const svgToUtf8 = (svg: ReactElement, props?: Prop) =>
+  toUtf8(svg, props);
 
-export const svgToDataUrl = (svg: ReactElement, props?: Prop) =>
-  `url('${toData(svg, props)}')`;
+export const svgToBase64 = (svg: ReactElement, props?: Prop) =>
+  toBase64(svg, props);
+
+export const svgToUtf8Url = (svg: ReactElement, props?: Prop) =>
+  `url('${toUtf8(svg, props)}')`;
+
+export const svgToBase64Url = (svg: ReactElement, props?: Prop) =>
+  `url('${toBase64(svg, props)}')`;
