35d5e94b07777f627475cbe71b6a2a0ff71806aa
ENPANADA <92269511+ENPANADA@users.noreply.github.com>
2025-02-06 10:46:00 -0600

    Home Page 🛥️

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,5 +1,5 @@
 export const ROOT_URL = '/Sharkit'; // to deploy(GitHub)
-export const HOME_URL = ROOT_URL + '/';
+export const HOME_URL = ROOT_URL + '/home';
 export const HOME_COMPONENT_URL = ROOT_URL + '/components';
 export const HOME_BACKGROUND_URL = ROOT_URL + '/backgrounds';
 
@@ -8,5 +8,5 @@ export const COMPONENT_URL = HOME_COMPONENT_URL + '/';
 export const CODEOPENIO_BASE_URL = 'https://codepen.io/ENPANADA/pen/';
 export const CODILINK_BASE_URL = 'https://codi.link/';
 
-export const REPO_URL = 'https://github.com/ENPANADA/Sharkit';
 export const ENPANADA_URL = 'https://github.com/ENPANADA';
+export const REPO_URL = ENPANADA_URL + ROOT_URL;
